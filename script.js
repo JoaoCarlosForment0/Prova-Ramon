5e9cf194fac324f1bcfc9290b3f51d3b7980502c
JOÃO CARLOS FORMENTO <joao_c_formento@estudante.sesisenai.org.br>
2025-05-13 15:14:31 -0300

    Removido variavel mensagem inutilizada

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 const { v4: uuidv4 } = require("uuid");
 const fs = require("fs");
 
-const mensagem = "teste123";
-
-const escreveLog = (mensagem) => {
+const escreveLog = () => {
   return `\n<${uuidv4()}> <${new Date().toLocaleString()}> <Nome aluno>`;
   //A ser implementado variavel para receber o nome do aluno
 };
 
-fs.appendFile("logs.txt", escreveLog(mensagem), (err) => {
+fs.appendFile("logs.txt", escreveLog(), (err) => {
   if (err) throw err;
   console.log("Senha salva!");
 });
